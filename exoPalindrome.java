d2265faef1728e876787b2160b3e104e1eb616a5
aZeDen <lucasgosselin3101@gmail.com>
2025-04-01 12:41:32 +0200

    Exo palindrome reussi

--- exoPalindrome.java
+++ exoPalindrome.java
@@ -6,6 +6,23 @@ import static java.lang.System.*;
 public class exoPalindrome {
 
     public static void main(String... args) {
-        out.println("Hello World");
+        System.out.println(retourneString("Bonjour !"));
+        System.out.println(isPalindrome("radar"));
+        System.out.println(isPalindrome("Bonjour !"));
+    }
+
+    private static String retourneString(String  str){
+        String res = "";
+        for (int i = str.length()-1; i >= 0; i--){
+            res += str.charAt(i);
+        }
+        return res;
+    }
+
+    private static String isPalindrome(String str){
+        if(str.equals(exoPalindrome.retourneString(str))){
+        return "Palindrome !";
+        }
+        return "Pas palindrome !";
     }
 }
