0f5e2aff8cfd46a71c80a2b2c6cecb901de9cd4a
aZeDen <lucasgosselin3101@gmail.com>
2025-04-08 10:39:31 +0200

    Modif isPrime pour pouvoir l'utiliser dans primeTable

--- isPrime.java
+++ isPrime.java
@@ -11,24 +11,40 @@ public class isPrime {
         System.out.println(estPremier());
     }
 
+    public static boolean estPrime(int nb){
+        if(nb == 2){
+            return true;
+        } else if(nb == 0 || nb == 1){
+            return false;
+        } else {
+            for(int i = 2 ; i <= nb/2+1 ; i++){
+                if(nb%i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+    }
+
     public static boolean estPremier(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Veuillez choisir un nombre :");
         int nb = sc.nextInt();
         if(nb == 2){
-            sc.close();
+            
             return true;
         } else if(nb == 0 || nb == 1){
-            sc.close();
+            
             return false;
         } else {
             for(int i = 2 ; i <= nb/2+1 ; i++){
                 if(nb%i == 0){
-                    sc.close();
+                    
                     return false;
                 }
             }
-            sc.close();
+            
             return true;
         }
         
--- primeTable.java
+++ primeTable.java
@@ -2,10 +2,25 @@
 
 
 import static java.lang.System.*;
+import java.util.ArrayList;
 
 public class primeTable {
 
     public static void main(String... args) {
-        out.println("Hello World");
+        int cnt = 0;
+        boolean bool = true;
+        ArrayList<Integer> nombres = new ArrayList<>();
+        int i = 0;
+        while(bool){
+            if(isPrime.estPrime(i)){
+                nombres.add(i);
+                cnt++;
+            }
+            if(cnt == 10){
+                bool = false;
+            }
+            i++;
+        }
+        System.out.println(nombres);
     }
 }
