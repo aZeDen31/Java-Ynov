99d21ea3f4992d8e5f912cbde642a1f970f1fa02
aZeDen <lucasgosselin3101@gmail.com>
2025-04-08 11:22:33 +0200

    Ajout Cricle

--- Circle.java
+++ Circle.java
@@ -5,7 +5,29 @@ import static java.lang.System.*;
 
 public class Circle {
 
+    private double x;
+    private double y;
+    private double rayon;
+
+    public Circle(double x, double y, double rayon){
+        this.x = x;
+        this.y = y;
+        this.rayon = rayon;
+    }
+
     public static void main(String... args) {
         out.println("Hello World");
+    }
+
+    public boolean isCollision(Circle c1, Circle c2){
+        if(c1.rayon+c2.rayon >= distanceBetween(c1, c2)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public double distanceBetween(Circle c1, Circle c2){
+        return Math.sqrt((c2.x-c1.x)*(c2.x-c1.x) + (c2.y-c1.y)*(c2.y-c1.y));
     }
 }
