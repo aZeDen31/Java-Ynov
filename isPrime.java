75498336eb195cd215754f1724350a2fc61ebc77
aZeDen <lucasgosselin3101@gmail.com>
2025-04-08 09:35:29 +0200

    Ajout isPrime

---
+++ isPrime.java
@@ -0,0 +1,36 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class isPrime {
+
+    public static void main(String... args) {
+        System.out.println(estPremier());
+    }
+
+    public static boolean estPremier(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez choisir un nombre :");
+        int nb = sc.nextInt();
+        if(nb == 2){
+            sc.close();
+            return true;
+        } else if(nb == 0 || nb == 1){
+            sc.close();
+            return false;
+        } else {
+            for(int i = 2 ; i <= nb/2+1 ; i++){
+                if(nb%i == 0){
+                    sc.close();
+                    return false;
+                }
+            }
+            sc.close();
+            return true;
+        }
+        
+    }
+}
